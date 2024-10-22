e6ea4dd4fd62c4c54cf36ce3eab7469e91204839
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-23 01:01:44 +0300

    add context intro

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+      firstName: firstName,
+      familyName: familyName,
+      title: title,
+      payPerHour: payPerHour,
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  // Function to create multiple employee records
+  function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+  }
+
+  function createTimeInEvent() {
+    
+  }
